3f44257848584243dd9013de7b34b9261856e1b4
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-10-09 22:48:27 +0200

    feat: Update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,7 @@ Rails.application.configure do
   config.action_mailer.asset_host = ENV['REGEULAR_DOMAIN']
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
     address: Rails.application.credentials.dig(:aws, :ses, :address),
