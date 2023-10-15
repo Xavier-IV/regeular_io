92e705c51025de7dc498fbe6eb9cd981dde139bf
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-10-15 12:39:59 +0200

    fix: Lint

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -76,7 +76,6 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
 
-
   config.action_mailer.smtp_settings = {
     address: Rails.application.credentials.dig(:aws, :ses, :address),
     port: 587,
