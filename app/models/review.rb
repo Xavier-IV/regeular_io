65e63afdcd822b8b967189db68ec57bc0fdbdd4b
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-10-28 00:31:28 +0200

    feat: Update fixtures

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@
 
 class Review < ApplicationRecord
   self.table_name = 'reviews'
+  belongs_to :business
   belongs_to :customer, optional: true, foreign_key: 'user_id', inverse_of: 'qr_code_review'
   has_one :qr_code_review, class_name: 'QrCode', dependent: :destroy
   # belongs_to :business
