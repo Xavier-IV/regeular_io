8f84e4e4e33d0df408031c9476ae5646f74e09be
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-10-28 08:14:08 +0200

    fix: Lint

--- db/migrate/20231027224740_add_unique_index_to_user_email_and_type.rb
+++ db/migrate/20231027224740_add_unique_index_to_user_email_and_type.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddUniqueIndexToUserEmailAndType < ActiveRecord::Migration[7.0]
   def change
-    add_index :users, [:email, :type], unique: true
+    add_index :users, %i[email type], unique: true
   end
 end
