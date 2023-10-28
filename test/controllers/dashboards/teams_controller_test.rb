aa05e7747d1d7ef235fa93bf104e9c43ec2d0cac
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-10-28 12:21:59 +0200

    fix: Update spec

--- test/controllers/dashboards/teams_controller_test.rb
+++ test/controllers/dashboards/teams_controller_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class Dashboards::TeamsControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get dashboards_teams_index_url
-    assert_response :success
+    get dashboards_teams_url
+    assert_response 302
   end
 end
