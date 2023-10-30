3685f9a04be9df666bf95ae641d0fb628966a4e7
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-10-30 23:18:39 +0100

    fix: Lint

--- app/components/modal_component.rb
+++ app/components/modal_component.rb
@@ -2,6 +2,7 @@
 
 class ModalComponent < ViewComponent::Base
   def initialize(title:)
+    super
     @title = title
   end
 end
--- db/migrate/20231030212242_remove_level_from_customer_progress.rb
+++ db/migrate/20231030212242_remove_level_from_customer_progress.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class RemoveLevelFromCustomerProgress < ActiveRecord::Migration[7.0]
   def change
-    remove_column :customer_progresses, :level
-
+    remove_column :customer_progresses, :level, :string
   end
 end
