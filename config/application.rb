e5721f47284db3571bacbed514b07b7070cdb803
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-11-02 00:45:21 +0100

    fix: Lint

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,6 @@ module Regeular
 
     config.active_model.i18n_customize_full_message = true
 
-    config.exceptions_app = self.routes
+    config.exceptions_app = routes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  match "/404", to: "errors#not_found", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 
   namespace :customers do
     namespace :dashboards do
