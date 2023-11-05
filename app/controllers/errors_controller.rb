615e030ba4c80e600dfa1c02e08b838023846c82
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-11-05 14:52:40 +0100

    fix: Error handling

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,10 +2,18 @@
 
 class ErrorsController < ApplicationController
   def not_found
-    render status: :not_found
+    respond_to do |format|
+      format.html { render status: :not_found }
+      format.json { render json: { error: 'Not Found' }, status: :not_found }
+      format.all { head :not_found }
+    end
   end
 
   def internal_server_error
-    render status: :internal_server_error
+    respond_to do |format|
+      format.html { render status: :internal_server_error }
+      format.json { render json: { error: 'Internal Server Error' }, status: :internal_server_error }
+      format.all { head :internal_server_error }
+    end
   end
 end
