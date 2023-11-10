f649276d8cd34ff2be94b7a84965c16fbf17469c
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-11-10 19:12:43 +0100

    RB-10 fix: Safe check

--- app/controllers/dashboards/progresses_controller.rb
+++ app/controllers/dashboards/progresses_controller.rb
@@ -2,11 +2,17 @@
 
 class Dashboards::ProgressesController < ApplicationController
   def create
+    return redirect_to dashboards_path unless %w[onboarding.get_started
+                                                 onboarding.verified_email
+                                                 onboarding.has_logo
+                                                 onboarding.has_listing_image
+                                                 onboarding.has_operating_hours
+                                                 onboarding.has_gmap_link].include?(params[:progress_type].to_s)
+
     current_client.client_progresses.create(
       progress_type: params[:progress_type],
       completed_at: Time.zone.now
     )
-    # return unless progress.persisted? && progress.progress_type == 'onboarding.get_started'
 
     redirect_to dashboards_path
   end
