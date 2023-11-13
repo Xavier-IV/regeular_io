12cf32c26b46861fcd42dfe2bf08a38f20d816c2
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-11-13 18:21:48 +0100

    RB-10 fix: Dashboard data

--- app/helpers/customer_helper.rb
+++ app/helpers/customer_helper.rb
@@ -2,8 +2,16 @@
 
 module CustomerHelper
   def add_pending_progress(current_customer, business)
+    ratings = current_customer.reviews.where(business_id: business.id)
+                              .map(&:rating)
+    rating_average = ratings.reduce(:+).to_f / ratings.length
+
     progress = Customer::Progress.find_or_create_by(user: current_customer, business:)
-    progress.rating_pending = count
+
+    progress.rating_average = rating_average
+    progress.rating_count = ratings.length
     progress.save
+
+    progress.gain_exp
   end
 end
