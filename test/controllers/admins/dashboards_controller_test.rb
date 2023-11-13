ada26b052057dbd5bce0ce1f72a50c61c784a497
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-11-13 20:20:00 +0100

    RB-10 fix: Spec

--- test/controllers/admins/dashboards_controller_test.rb
+++ test/controllers/admins/dashboards_controller_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class Admins::DashboardsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers # Include Devise test helpers
+
+  test 'should get index' do
+    sign_in admins(:admin_one)
+
+    get admins_dashboard_url
+    assert_response 200
+  end
 end
