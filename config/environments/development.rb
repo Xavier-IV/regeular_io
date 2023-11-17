e0da7bdfb154ac6723f673e310d8d889e45bbf5d
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-11-17 10:35:11 +0100

    fix: Pivot domain priority

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << 'regeular-local.com'
-  config.hosts << 'business.regeular-local.com'
+  config.hosts << 'customer.regeular-local.com'
   config.hosts << 'admin.regeular-local.com'
 
   # In the development environment your application's code is reloaded any time
