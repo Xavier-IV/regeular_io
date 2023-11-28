8935a4931d5add9d6e1fc4292ea37fa312526835
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-11-28 21:10:48 +0100

    fix: Hide in prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,34 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  match '/404', to: 'errors#not_found', via: :all
-  match '/500', to: 'errors#internal_server_error', via: :all
+  match '*path', to: 'notices#construction', via: :all
 
-  get '/health', to: 'health_checks#health'
+  root 'notices#construction' if Rails.env.production?
 
-  get '/states', to: 'geographies#states', defaults: { format: :json }
-  get '/cities', to: 'geographies#cities', defaults: { format: :json }
+  unless Rails.env.production?
+    draw(:omniauth)
+    draw(:admin)
+    draw(:customer)
+    draw(:business)
 
-  get '/terms_and_conditions', to: 'landings/legals#terms_and_conditions'
-  get '/cookie_policy', to: 'landings/legals#cookie_policy'
-  get '/privacy_policy', to: 'landings/legals#privacy_policy'
-  get '/acceptable_use_policy', to: 'landings/legals#acceptable_use_policy'
-  get '/disclaimer', to: 'landings/legals#disclaimer'
+    match '/404', to: 'errors#not_found', via: :all
+    match '/500', to: 'errors#internal_server_error', via: :all
 
-  match '*path', to: 'notices#construction', via: :all, constraints: ->(req) { !req.xhr? } if Rails.env.production?
+    get '/health', to: 'health_checks#health'
 
-  get '/construction', to: 'notices#construction'
+    get '/states', to: 'geographies#states', defaults: { format: :json }
+    get '/cities', to: 'geographies#cities', defaults: { format: :json }
 
-  post '/clients/webhook', to: 'clients/webhooks#stripe'
+    get '/terms_and_conditions', to: 'landings/legals#terms_and_conditions'
+    get '/cookie_policy', to: 'landings/legals#cookie_policy'
+    get '/privacy_policy', to: 'landings/legals#privacy_policy'
+    get '/acceptable_use_policy', to: 'landings/legals#acceptable_use_policy'
+    get '/disclaimer', to: 'landings/legals#disclaimer'
 
-  post 'pwa_subscriptions', to: 'pwa_subscriptions#create'
+    get '/construction', to: 'notices#construction'
 
-  draw(:omniauth)
-  draw(:admin)
-  draw(:customer)
-  draw(:business)
+    post '/clients/webhook', to: 'clients/webhooks#stripe'
+
+    post 'pwa_subscriptions', to: 'pwa_subscriptions#create'
+  end
 end
